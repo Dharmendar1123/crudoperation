74a5cdd2b2dd130968f487b0894e92b0442a10d7
Parmar Dharmendar <dharmendar1123@gmail.com>
2021-09-28 21:23:49 +0530

    formik validate

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,24 +2,50 @@ import React, { useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import axios from "axios";
 import { useHistory } from "react-router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  fname: "",
+  lname: "",
+  email: "",
+};
+
+const onSubmit = (values) => {
+  console.log("form data", values);
+};
+
+const validationSchema = Yup.object({
+  fname: Yup.string().required("Required!"),
+  lname: Yup.string().required("Required!"),
+  email: Yup.string().email("Invalid email format").required("Required!"),
+});
 
 function Create() {
   let history = useHistory();
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [fname, setFirstName] = useState("");
+  const [lname, setLastName] = useState("");
   const [email, setEmail] = useState("");
 
   const postData = () => {
-    axios
-      .post(`https://6152a1214a5f22001701d7ab.mockapi.io/fakerData`, {
-        firstName,
-        lastName,
-        email,
-      })
-      .then(() => {
-        history.push("/read");
-      });
+    if (fname !== "" && lname !== "" && email !== "") {
+      axios
+        .post(`https://6152a1214a5f22001701d7ab.mockapi.io/fakerData`, {
+          fname,
+          lname,
+          email,
+        })
+        .then(() => {
+          history.push("/read");
+        });
+    }
   };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+  });
 
   return (
     <div>
@@ -31,7 +57,11 @@ function Create() {
             name="fname"
             placeholder="First Name"
             onChange={(e) => setFirstName(e.target.value)}
+            {...formik.getFieldProps("fname")}
           />
+          {formik.touched.fname && formik.errors.fname ? (
+            <div className="error">{formik.errors.fname}</div>
+          ) : null}
         </Form.Field>
 
         <Form.Field>
@@ -41,7 +71,11 @@ function Create() {
             name="lname"
             placeholder="Last Name"
             onChange={(e) => setLastName(e.target.value)}
+            {...formik.getFieldProps("lname")}
           />
+          {formik.touched.lname && formik.errors.lname ? (
+            <div className="error">{formik.errors.lname}</div>
+          ) : null}
         </Form.Field>
 
         <Form.Field>
@@ -51,14 +85,14 @@ function Create() {
             name="email"
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
+            {...formik.getFieldProps("email")}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error">{formik.errors.email}</div>
+          ) : null}
         </Form.Field>
 
-        <Button
-          onClick={postData}
-          type="submit"
-          disabled={firstName === "" || lastName === "" || email === ""}
-        >
+        <Button onClick={postData} type="submit">
           Submit
         </Button>
       </Form>
